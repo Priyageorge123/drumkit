8698725fb6679bbd7fbf79113dab6a8da6a0ba5c
Priyageorge123 <36256161+Priyageorge123@users.noreply.github.com>
2020-07-30 18:45:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,52 @@
+for (var i = 0; document.querySelectorAll(".drum").length; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    var buttontext = this.innerHTML;
+    activate(buttontext);
+    animation(buttontext);
+  });
+
+  document
+    .querySelectorAll(".drum")
+    [i].addEventListener("keydown", function(event) {
+      activate(event.key);
+      animation(event.key);
+    });
+}
+function activate(buttontext) {
+  switch (buttontext) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "d":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+    case "j":
+      var tom5 = new Audio("sounds/snare.mp3");
+      tom5.play();
+      break;
+    case "k":
+      var tom6 = new Audio("sounds/kick-bass.mp3");
+      tom6.play();
+      break;
+    case "l":
+      var tom7 = new Audio("sounds/crash.mp3");
+      tom7.play();
+      break;
+  }
+}
+function animation(buttontext) {
+  document.querySelector("." + buttontext).classList.add("pressed");
+  setTimeout(function() {
+    document.querySelector("." + buttontext).classList.remove("pressed");
+  }, 100);
+}
